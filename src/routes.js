513d9436a618bb8b22fc6d6746bd01685c843271
Colin <46408738+colinernds@users.noreply.github.com>
2021-03-12 14:29:01 +0100

    Ajout de fonctionnalités (V2)
    -Pages Demo, About us, contact
    - NavBar Responsive (en cours) Hamburger Dropdown menu
    - Page Home/Fonctionnalités
    -Component Button
    -Ajout du logo svg (pas mis en place)

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,9 @@ import {Route, Switch} from 'react-router-dom';
 
 import Home from './pages/home';
 import Team from './pages/team';
+import Demo from './pages/demo';
+import AboutUs from './pages/about-us';
+import Contact from './pages/contact';
 
 // -----------------------------------------------------------------------------
 
@@ -13,6 +16,9 @@ const Routes = () => (
   <Switch>
     <Route path="/" exact component={Home} />
     <Route path="/team" exact component={Team} />
+    <Route path="/demo" exact component={Demo} />
+    <Route path="/about-us" exact component={AboutUs} />
+    <Route path="/contact" exact component={Contact} />
   </Switch>
 );
 
