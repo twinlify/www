ad45bd2e7d674638183734519dae6968501d11ab
Colin <46408738+colinernds@users.noreply.github.com>
2021-03-15 10:31:06 +0100

    Corrections Breakpoints,/Colors

--- src/components/button.js
+++ src/components/button.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
 import {useHistory} from 'react-router';
-import  {colors} from '../style/colors';
+import  {green, darkGreen} from '../style/colors';
 
 //---------------------------------------------------
 
 const $Button = styled.div`
 padding: 1rem 2rem;
-background: ${colors.green};
+background: ${green};
 max-width: 20ch;
 max-height: 30px;
 border-radius: 30px;
 cursor: pointer;
 
 &:hover {
-    background: ${colors.darkGreen};
+    background: ${darkGreen};
 }
 `
 //---------------------------------------------------
--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import styled from 'styled-components';
 import {useHistory} from 'react-router';
 import {device, mediaQueryTransition} from '../style/breakpoints';
 import logo from '../img/logo_square.svg';
-import {colors} from '../style/colors';
+import {darkGray, primaryBlue} from '../style/colors';
 // -----------------------------------------------------------------------------
 
 const $Header = styled.header`
@@ -16,7 +16,7 @@ const $Header = styled.header`
   padding: 0 20px;
   box-sizing: border-box;
   height: 100px;
-  background: ${colors.black};
+  background: ${darkGray};
   display: flex;
   flex-wrap: wrap;
   align-items: center;
@@ -29,48 +29,57 @@ const $Logo = styled.img`
 
 const $Links = styled.div`
   display: flex;
-  font-size: 1.3rem;
+  overflow: hidden;
+  top: 100px;
+  left: 0;
+  width: 100%;
+  background: rgb(88,88,88);
+  background: linear-gradient(0deg, rgba(88,88,88,1) 0%, rgba(40,40,40,1) 54%, rgba(17,17,17,1) 100%);
+  flex-direction: column;
+  font-size: 1.5rem;
+  max-height: 0px;
+  transition: max-height 0.3s ease-in;
+  }
+  & .active {
+    max-height: 300px;
+  }
 
-  @media ${device.laptopMax} {
-    overflow: hidden;
-    top: 100px;
-    left: 0;
-    width: 100%;
-    background: rgb(88,88,88);
-    background: linear-gradient(0deg, rgba(88,88,88,1) 0%, rgba(40,40,40,1) 54%, rgba(17,17,17,1) 100%);
-    flex-direction: column;
-    font-size: 1.5rem;
-    max-height: ${({ isOpen }) => (isOpen ? "300" : "0")};
-    transition: max-height 0.3s ease-in;
+  @media ${device.laptop} {
+   top: 0px;
+   width: auto;
+   max-height: unset;
+   background: ${darkGray};
+   flex-direction: row;
+   font-size: 1.3rem;
   }
 `;
 
 const $Link = styled.p`
-  margin: 0 20px;
+  margin: 20px 0;
   cursor: pointer;
 
   &:hover {
-    color: ${colors.primaryBlue};
+    color: ${primaryBlue};
   }
 
-  @media ${device.laptopMax} {
-    margin: 20px 0;
+  @media ${device.laptop} {
+    margin: 0 20px;
   }
 `;
 
-const Hamburger = styled.div`
-  display: none;
+const $Hamburger = styled.div`
+  display: flex;
   flex-direction: column;
   cursor: pointer;
   span {
     height: 5px;
     width: 30px;
-    background: ${({ isOpen }) => (isOpen=true ? "red" : "white")};
+    background: ${(isOpen) => (isOpen ? "red" : "white")};
     border-radius: 5px;
     &:not(:last-child) {margin-bottom: 6px}
   }
-  @media ${device.laptopMax} {
-    display: flex;
+  @media ${device.laptop} {
+    display: none;
   }
 `;
 
@@ -106,16 +115,17 @@ const Header = () => {
     console.log(isOpen); //a enlever
   };
 
+
   return (
     <$Header>
       <$Logo src="https://static.twinlify.com/logos/logo.svg" />
       {/*<$Logo src={logo} />*/}
-      <Hamburger onClick={handleClick}>
+      <$Hamburger onClick={handleClick}>
         <span />
         <span />
         <span />
-      </Hamburger>
-      <$Links>
+      </$Hamburger>
+      <$Links className={isOpen? "active" : ""}>
         <$Link onClick={openHome}>Home</$Link>
         <$Link onClick={openTeam}>Team</$Link>
         <$Link onClick={openDemo}>Demo</$Link>
