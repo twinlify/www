8429e30e6091748f0762bad64aaff292d9049c2f
Colin <46408738+colinernds@users.noreply.github.com>
2021-03-30 19:35:26 +0200

    Conflit

--- src/components/header.js
+++ src/components/header.js
@@ -4,10 +4,10 @@ import React, {useState} from 'react';
 import styled, {css} from 'styled-components';
 import {useHistory} from 'react-router';
 import {device} from '../style/breakpoints';
-import {white, lightGray, darkGray, primaryBlue} from '../style/colors';
-import WifiLogo from '../img/wi-fi.svg';
-import RightArrow from '../img/right-arrow.svg';
+import {lightGray, darkGray, primaryBlue} from '../style/colors';
 import Hamburger from './hamburger';
+import Solutions from './solutions';
+
 // -----------------------------------------------------------------------------
 
 const $Header = styled.header`
@@ -21,7 +21,7 @@ const $Header = styled.header`
   background: ${darkGray};
   display: flex;
   align-items: center;
-  justify-content: center; 
+  justify-content: center;
 `;
 
 const $Nav = styled.nav`
@@ -114,64 +114,6 @@ const $Link = styled.li`
   }
 `;
 
-const $HoverMenu = styled.ul`
-  display: none;
-  flex-direction: column;
-  align-items: flex-start;
-  width: 300px;
-  background: ${white};
-  list-style: none;
-  position: absolute;
-  top: 55px;
-  padding-inline-start: 0px;
-  padding: 1rem;
-  // small square
-  &::before {
-    content: "";
-    z-index: -1;
-    width: 20px;
-    height: 20px;
-    background: ${white};
-    position: absolute;
-    top: -5px;
-    left: 40px;
-    transform: rotate(45deg);
-  }
-  ${props => props.isVisibleSolutions &&
-  css`
-    display: flex;
-  `}
-`;
-
-const $HoverLink = styled.li`
-  color: ${darkGray};
-  padding: 0.5rem;
-  font-size: 1rem;
-  cursor: pointer;
-  svg {
-    margin-right: 5px;
-  }
-  &:hover {
-    color: ${primaryBlue};
-  }
-`;
-
-const $LinkRedirect = styled.span`
-  padding-top: 10px;
-  cursor: pointer;
-  color: ${primaryBlue};
-  svg {
-    margin-left: 10px;
-    transform: translateX(0) translateY(2px);
-    transition: all 300ms ease-in;
-  }
-  &:hover {
-    svg {
-      transform: translateX(10px) translateY(2px);
-    }
-  }
-`;
-
 // -----------------------------------------------------------------------------
 
 const Header = () => {
@@ -210,7 +152,6 @@ const Header = () => {
 
   const hoverIn = () => {
     setIsVisibleSolutions(true);
-    console.log(isVisibleSolutions)
   }
 
   const hoverOut = () => {
@@ -229,12 +170,7 @@ const Header = () => {
           <$Link onClick={openHome}>Home</$Link>
           <$LinkWrapper onMouseEnter={hoverIn} onMouseLeave={hoverOut}>
             <$Link onClick={openSolutions}>Solutions</$Link>
-            <$HoverMenu isVisibleSolutions={isVisibleSolutions}>
-              <$HoverLink><WifiLogo width="20" />Asset tracking</$HoverLink>
-              <$HoverLink><WifiLogo width="20" />Real time maps</$HoverLink>
-              <$HoverLink><WifiLogo width="20" />Interactive storytelling</$HoverLink>
-              <$LinkRedirect onClick={openSolutions}>All Solutions<RightArrow width="20"/></$LinkRedirect>
-            </$HoverMenu>
+            <Solutions isVisibleSolutions={isVisibleSolutions}/>
           </$LinkWrapper>
           <$Link onClick={openDemo}>Demo</$Link>
           <$Link onClick={openAboutUs}>About us</$Link>
