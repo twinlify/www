b696d28af84a76c8c32bfd216163e570d735f40c
Colin <46408738+colinernds@users.noreply.github.com>
2021-04-15 15:54:56 +0200

    v1

--- src/pages/solutions/index.js
+++ src/pages/solutions/index.js
@@ -1,5 +1,24 @@
 import React from 'react';
+import styled from 'styled-components';
+import Nexus from '@twinlify/nexus';
 
-const Solutions = () => <p>Solutions</p>;
+Nexus.create({
+  clientId: 'demo',
+  containerId: 'nexusContainer',
+  configId: 'rooms'
+});
+
+const $Nexus = styled.div`
+    width: 600px;
+    height: 450px;
+`
+const Solutions = () => {
+    return (
+        <main>
+        <p>Solutions</p>
+        <$Nexus id="nexusContainer"/>
+        </main>
+    )
+}
 
 export default Solutions;
